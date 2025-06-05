ce6ccf5224c782ceff1358741d127f45be7a0a6c
Renan Freitas <renan.freitas.anjos@gmail.com>
2025-06-05 13:11:08 -0300

    feat: add routes for delete, update, listAll and FindById to Cliente

--- api/src/main/java/br/ifpe/edu/renanfreitas/modelo/cliente/ClienteService.java
+++ api/src/main/java/br/ifpe/edu/renanfreitas/modelo/cliente/ClienteService.java
@@ -1,5 +1,7 @@
 package br.ifpe.edu.renanfreitas.modelo.cliente;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +17,27 @@ public class ClienteService {
         cliente.setHabilitado(Boolean.TRUE);
         return repository.save(cliente);
     }
+
+    @Transactional
+    public void update(Long id, Cliente clienteAlterado){
+        Cliente cliente = repository.findById(id).get();
+        cliente.setNome(clienteAlterado.getNome());
+        cliente.setCpf(clienteAlterado.getCpf());
+        cliente.setRg(clienteAlterado.getRg());
+        cliente.setDataNascimento(clienteAlterado.getDataNascimento());
+        cliente.setFoneCelular(clienteAlterado.getFoneCelular());
+
+        repository.save(cliente);
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Cliente cliente = repository.findById(id).get();
+        cliente.setHabilitado(Boolean.FALSE);
+
+        repository.save(cliente);
+    }
+
+    public List<Cliente> listarTodos(){ return repository.findAll(); }
+    public Cliente obterPorId(Long id){ return repository.findById(id).get(); }
 }
